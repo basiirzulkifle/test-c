58d3129f0dcf9729ed304b206680a6764873626f
M.Basiir <md.basiir@i-smart.com.sg>
2023-11-07 11:32:18 +0800

    fix staffcontroller

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
-use App\Models\Staff;
+use App\Models\User;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class StaffController extends Controller
 {
-    
+
     public function store(Request $request)
     {
-        $staff = new Staff;
+        $staff = new User;
         $staff->name = $request->name;
         $staff->email = $request->email;
         $staff->mobile_no = $request->mobile_no;
         $staff->user_id = $request->user_id;
         $staff->base64_text = $request->base64_text;
         $staff->save();
-        return response()->json(['message'=>'Staff register succesfully']);
-        
+        return response()->json(['message' => 'Staff register succesfully']);
+
     }
 }
